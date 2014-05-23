76e482ebc3dfe5f2946d88ffd8dbe55b1fd1939b
Jason Yu <yujason2@gmail.com>
2014-05-23 04:47:40 -0700

    Pseudocode.

---
+++ anagrams.rb
@@ -0,0 +1,8 @@
+if __FILE__ == $0
+  # go through wordlist
+  # for each word, create a hash word_hash of {sorted_letters => word}
+  # go through each word of word_hash
+  # if the input is a subword of it
+  # add the word
+end
+    
